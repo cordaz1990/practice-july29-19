7e056cb08ac93a4701cf7e3ae3f2c085b984d509
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:45:14 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,19 @@
-print('hello')
-print('world')
-print('hello')
+#List
+
+cheese = ['cheddar', 'Edam', 'Gouda']
+numbers = [42, 123]
+empty = []
+print(cheeses, numbers, empty)
+['cheddar', 'Edam', 'Gouda'] [42, 123] []
+
+numbers = [42, 123]
+numbers[1] = 5
+numbers
+[42, 5]
+
+cheese = ['Cheddar', 'Edam', 'Gouda']
+'Edam' in cheeses
+True
+
+'Brie' in cheeses
+False
